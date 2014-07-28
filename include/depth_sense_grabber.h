481d020b2995d4a7d7de3cf7ed06618ae0bd2e7d
Sergey Alexandrov <alexandrov88@gmail.com>
2014-07-28 23:02:47 +0200

    Add documentation for DepthSenseGrabber

--- include/depth_sense_grabber.h
+++ include/depth_sense_grabber.h
@@ -60,17 +60,30 @@ namespace pcl
     }
   }
 
+  /** \brief A helper class to measure frequency of a certain event.
+    *
+    * To use this class create an instance and call event() function every time
+    * the event in question occurs. The estimated frequency can be retrieved
+    * with getFrequency() function.
+    *
+    * \ingroup common
+    */
   class EventFrequency
   {
 
     public:
 
+      /** \brief Constructor.
+        *
+        * \param[i] window_size number of most recent events that are
+        * considered in frequency estimation (default: 30) */
       EventFrequency (size_t window_size = 30)
       : window_size_ (window_size)
       {
         stop_watch_.reset ();
       }
 
+      /** \brief Notifies the class that the event occured. */
       void event ()
       {
         event_time_queue_.push (stop_watch_.getTimeSeconds ());
@@ -78,6 +91,7 @@ namespace pcl
           event_time_queue_.pop ();
       }
 
+      /** \brief Retrieve the estimated frequency. */
       double
       getFrequency () const
       {
@@ -87,6 +101,7 @@ namespace pcl
                 (event_time_queue_.back () - event_time_queue_.front ()));
       }
 
+      /** \brief Reset frequency computation. */
       void reset ()
       {
         stop_watch_.reset ();
@@ -155,6 +170,15 @@ namespace pcl
       virtual float
       getFramesPerSecond () const;
 
+      /** Set the confidence threshold for depth data.
+        *
+        * Each pixel in a depth image output by the device has an associated
+        * confidence value. The higher this value is, the more reliable the
+        * datum is.
+        *
+        * The depth pixels (and their associated 3D points) are filtered based
+        * on the confidence value. Those that are below the threshold are
+        * discarded (i.e. their coordinates are set to NaN). */
       void
       setConfidenceThreshold (int threshold);
 
@@ -166,27 +190,43 @@ namespace pcl
       void
       configureColorNode (DepthSense::ColorNode node) const;
 
+      /** A callback for processing depth data.
+        *
+        * It is supposed to be called from the DepthSense::Context thread that
+        * is managed by DepthSenseDeviceManager. */
       void
       onDepthDataReceived (DepthSense::DepthNode node, DepthSense::DepthNode::NewSampleReceivedData data);
 
+      /** A callback for processing color data.
+        *
+        * It is supposed to be called from the DepthSense::Context thread that
+        * is managed by DepthSenseDeviceManager. */
       void
       onColorDataReceived (DepthSense::ColorNode node, DepthSense::ColorNode::NewSampleReceivedData data);
 
+      // The manager should be able to invoke callbacks and configuration
       friend pcl::io::depth_sense::DepthSenseDeviceManager;
 
-      // signals to indicate whether new clouds are available
+      // Signals to indicate whether new clouds are available
       boost::signals2::signal<sig_cb_depth_sense_point_cloud>* point_cloud_signal_;
       boost::signals2::signal<sig_cb_depth_sense_point_cloud_rgba>* point_cloud_rgba_signal_;
 
+      /// Serial number of the device captured by this grabber
       std::string device_id_;
 
       bool is_running_;
 
       int confidence_threshold_;
 
+      /// Indicates whether there are subscribers for PointXYZ signal. This is
+      /// computed and stored on start()
       bool need_xyz_;
+
+      /// Indicates whether there are subscribers for PointXYZRGBA signal. This
+      /// is computed and stored on start()
       bool need_xyzrgba_;
 
+      /// Temporary buffer to store color data
       std::vector<uint8_t> color_data_;
 
       EventFrequency frequency_;
