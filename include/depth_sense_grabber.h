cdce0cbc448e79680082062a7b9467fc7c4e6ffc
Sergey Alexandrov <alexandrov88@gmail.com>
2014-09-14 14:50:10 +0200

    Move time-related functions into a separate file

--- include/depth_sense_grabber.h
+++ include/depth_sense_grabber.h
@@ -45,9 +45,11 @@
 #include <pcl/io/grabber.h>
 
 #include <queue>
-#include <pcl/common/time.h>
 
 #include <DepthSense.hxx>
+
+// TODO: remove when mering upstream
+#include <depth_sense/time.h>
 
 namespace pcl
 {
@@ -60,62 +62,6 @@ namespace pcl
       class Buffer;
     }
   }
-
-  /** \brief A helper class to measure frequency of a certain event.
-    *
-    * To use this class create an instance and call event() function every time
-    * the event in question occurs. The estimated frequency can be retrieved
-    * with getFrequency() function.
-    *
-    * \ingroup common
-    */
-  class EventFrequency
-  {
-
-    public:
-
-      /** \brief Constructor.
-        *
-        * \param[i] window_size number of most recent events that are
-        * considered in frequency estimation (default: 30) */
-      EventFrequency (size_t window_size = 30)
-      : window_size_ (window_size)
-      {
-        stop_watch_.reset ();
-      }
-
-      /** \brief Notifies the class that the event occured. */
-      void event ()
-      {
-        event_time_queue_.push (stop_watch_.getTimeSeconds ());
-        if (event_time_queue_.size () > window_size_)
-          event_time_queue_.pop ();
-      }
-
-      /** \brief Retrieve the estimated frequency. */
-      double
-      getFrequency () const
-      {
-        if (event_time_queue_.size () < 2)
-          return (0.0);
-        return ((event_time_queue_.size () - 1) /
-                (event_time_queue_.back () - event_time_queue_.front ()));
-      }
-
-      /** \brief Reset frequency computation. */
-      void reset ()
-      {
-        stop_watch_.reset ();
-        event_time_queue_ = std::queue<double> ();
-      }
-
-    private:
-
-      pcl::StopWatch stop_watch_;
-      std::queue<double> event_time_queue_;
-      const size_t window_size_;
-
-  };
 
   class PCL_EXPORTS DepthSenseGrabber : public Grabber
   {
