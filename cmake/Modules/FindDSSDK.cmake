218b8ce9b4620c292fabb5f2657f045b5589eb76
Sergey Alexandrov <alexandrov88@gmail.com>
2015-03-10 23:33:00 +0100

    Fix FindDSSDK.cmake for Linux

--- cmake/Modules/FindDSSDK.cmake
+++ cmake/Modules/FindDSSDK.cmake
@@ -11,24 +11,34 @@
 #  DSSDK_LIBRARIES             Libraries needed to use DepthSense SDK
 
 find_path(DSSDK_DIR include/DepthSenseVersion.hxx
-          PATHS "$ENV{DEPTHSENSESDK32}"
-		        "$ENV{DEPTHSENSESDK64}"
-		        "$ENV{PROGRAMFILES}/SoftKinetic/DepthSenseSDK"
-				"$ENV{PROGRAMW6432}/SoftKinetic/DepthSenseSDK"
-				"C:/Program Files (x86)/SoftKinetic/DepthSenseSDK"
-				"C:/Program Files/SoftKinetic/DepthSenseSDK"
+          HINTS "$ENV{DEPTHSENSESDK32}"
+                "$ENV{DEPTHSENSESDK64}"
+          PATHS "$ENV{PROGRAMFILES}/SoftKinetic/DepthSenseSDK"
+                "$ENV{PROGRAMW6432}/SoftKinetic/DepthSenseSDK"
+                "C:/Program Files (x86)/SoftKinetic/DepthSenseSDK"
+                "C:/Program Files/SoftKinetic/DepthSenseSDK"
+                "/opt/softkinetic/DepthSenseSDK"
           DOC "DepthSense SDK directory")
 
 if(DSSDK_DIR)
 
   # Include directories
   set(DSSDK_INCLUDE_DIRS ${DSSDK_DIR}/include)
+  mark_as_advanced(DSSDK_INCLUDE_DIRS)
 
   # Libraries
-  set(DSSDK_LIBRARIES ${DSSDK_LIBRARY})
-  find_library(DSSDK_LIBRARY
-               NAMES DepthSense.lib
-               PATHS "${DSSDK_DIR}/lib/" NO_DEFAULT_PATH)
+  if(MSVC)
+    set(DSSDK_LIBRARIES_NAMES DepthSense)
+  else()
+    set(DSSDK_LIBRARIES_NAMES DepthSense DepthSensePlugins turbojpeg usb-1.0)
+  endif()
+  foreach(LIB ${DSSDK_LIBRARIES_NAMES})
+    find_library(DSSDK_LIBRARY_${LIB}
+                 NAMES "${LIB}"
+                 PATHS "${DSSDK_DIR}/lib/" NO_DEFAULT_PATH)
+    list(APPEND DSSDK_LIBRARIES ${DSSDK_LIBRARY_${LIB}})
+    mark_as_advanced(DSSDK_LIBRARY_${LIB})
+  endforeach()
 
   # Version
   set(DSSDK_VERSION 0)
@@ -38,30 +48,15 @@ if(DSSDK_DIR)
     set(DSSDK_VERSION "${CMAKE_MATCH_1}")
   endif()
   unset(_dsversion_H_CONTENTS)
-  
-  set(DSSDK_FOUND TRUE)
 
 endif()
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(DSSDK DEFAULT_MSG
-                                  DSSDK_LIBRARY DSSDK_INCLUDE_DIRS)
-
-mark_as_advanced(DSSDK_LIBRARY DSSDK_INCLUDE_DIRS)
-
-if(DSSDK_FOUND)
-  if(NOT DSSDK_FIND_QUIETLY)
-    message(STATUS "DepthSense SDK found (include: ${DSSDK_INCLUDE_DIRS}, libs: ${DSSDK_LIBRARIES})")
-    message(STATUS "DepthSense SDK version: ${DSSDK_VERSION}")	
-  endif()
-else()
-  if(DSSDK_FIND_REQUIRED)
-    message(FATAL_ERROR "Could not find DepthSense SDK!")
-  elseif(NOT DSSDK_FIND_QUIETLY)
-    message(WARNING "Could not find DepthSense SDK!")
-	return()
-  endif()
-endif()
+find_package_handle_standard_args(DSSDK
+  FOUND_VAR DSSDK_FOUND
+  REQUIRED_VARS DSSDK_LIBRARIES DSSDK_INCLUDE_DIRS
+  VERSION_VAR DSSDK_VERSION
+)
 
 if(MSVC)
   set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} /NODEFAULTLIB:LIBCMTD")
